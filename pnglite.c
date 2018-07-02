d9794a9a840af80d9681d0d9062745310a3f6119
Alexander Sabourenkov <screwdriver@lxnt.info>
2018-07-02 22:19:47 +0300

    code style nits

--- pnglite.c
+++ pnglite.c
@@ -927,17 +927,19 @@ png_set_data(png_t* png, unsigned width, unsigned height, char depth,
     png_filter(png, filtered);
     png_write_ihdr(png);
 
-    if (png->color_type == PNG_INDEXED)
+    if (png->color_type == PNG_INDEXED) {
         if ((err = png_write_plte(png))) {
             png_free(filtered);
             return err;
         }
+    }
 
-    if (transparency)
+    if (transparency) {
         if ((err = png_write_trns(png))) {
             png_free(filtered);
             return err;
         }
+    }
 
     png_write_idats(png, filtered);
     png_free(filtered);
